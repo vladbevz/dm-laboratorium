9f24af62617bb5972088d88ad5ad9a93fd53f1ff
vbevz <122693446+vladbevz@users.noreply.github.com>
2025-12-02 20:28:48 +0100

    updates json

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
+    sourcemap: true,
   },
 });
